63484751f675425b00595543eb477c2e91a7e3a5
danielta18 <153893558+danielta18@users.noreply.github.com>
2024-11-13 20:28:01 +0000

    first commit

--- src/app.py
+++ src/app.py
@@ -30,13 +30,43 @@ def handle_hello():
 
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+    
+    return jsonify(members), 200
 
+@app.route('/members', methods=['POST'])
+def x():
+    member = request.json
+    jackson_family.add_member(member)
+    
+    members = jackson_family.get_all_members()
+   
+    return jsonify(members), 200
+    
+@app.route('/members/<int:member_id>', methods=['GET'])
+def y(member_id):
+    member = jackson_family.get_member(member_id)
+    return jsonify(member), 200
 
-    return jsonify(response_body), 200
+@app.route('/members/<int:member_id>', methods=['DELETE'])
+def z(member_id):
+    member = jackson_family.delete_member(member_id)
+    if member == None:
+        return 400, "member ID does not exist"
+    else: 
+        return jsonify(member), 200
+
+@app.route('/members/<int:member_id>', methods=['PATCH'])
+def a(member_id):
+    change_data = request.get_json()
+    member = jackson_family.patch_member(member_id, change_data)
+    return jsonify(member), 200
+
+@app.route('/members/<int:member_id>', methods=['PUT'])
+def b(member_id):
+    change_data = request.get_json()
+    member = jackson_family.put_member(member_id, change_data)
+    return jsonify(member), 200
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/datastructures.py
+++ src/datastructures.py
@@ -7,6 +7,7 @@ update this file to implement the following already declared methods:
 - get_member: Should return a member from the self._members list
 """
 from random import randint
+from flask import request
 
 class FamilyStructure:
     def __init__(self, last_name):
@@ -20,16 +21,50 @@ class FamilyStructure:
         return randint(0, 99999999)
 
     def add_member(self, member):
-        # fill this method and update the return
-        pass
+        member["id"]= self._generateId()
+        self._members.append(member)
 
     def delete_member(self, id):
-        # fill this method and update the return
-        pass
+        for i in range(0,len(self._members)):
+            if self._members[i]["id"] == id:
+                member = self._members.pop(i)
+                return f"{member['first_name']} deleted"
+        # for member in self._members:
+        #     if member["id"] == id:
+        #         self._members.remove(member)
+        #         return f"{member['first_name']} deleted"
+        return None
+        
+    # def patch_member(self, id):
+    #     for i in range(0,len(self._members)):
+    #         if self._members[i]["id"] == id:
+    #             member = self._members[i]
+    #         change_data = request.get_json()
+    #         member.update(change_data)
+    #         return member
+    #     return "Update failed"
+    def patch_member(self, id, change_data):
+        for i in range(0,len(self._members)):  
+            if self._members[i]["id"] == id:
+                member = self._members[i]
+                for key, value in change_data.items():
+                    member[key] = value
+                return member
+        return "Update failed"
+
+    def put_member(self, id, change_data):
+        for i in range(0,len(self._members)):  
+            if self._members[i]["id"] == id:
+                self._members[i] = change_data
+                return self._members[i]
+        return "update failed"
 
     def get_member(self, id):
-        # fill this method and update the return
-        pass
+        for member in self._members:
+            if member["id"] == id:
+                return member
+        return "member ID doesnt exist"
+    
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
